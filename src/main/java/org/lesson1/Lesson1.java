be1706cec6795e0ee284ea2b999bf27d45d15333
Evgeniy <46program@gmail.com>
2024-06-10 19:30:53 +0300

    lesson 2

--- src/main/java/org/lesson1/Lesson1.java
+++ src/main/java/org/lesson1/Lesson1.java
@@ -2,8 +2,7 @@ package org.lesson1;
 
 public class Lesson1 {
     public static void main(String[] args) {
-
-        System.out.println("Hello lesson1");
-
+        System.out.println("Hello world lesson1 ");
+        System.out.println("Hello new commit");
     }
 }
---
+++ src/main/java/org/lesson2/Lesson2.java
@@ -0,0 +1,9 @@
+package org.lesson2;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello lesson2");
+
+    }
+}
