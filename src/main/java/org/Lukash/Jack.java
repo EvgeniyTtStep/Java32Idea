30a015492f2e3ad0a81434004a8278d992f89d70
Evgeniy <46program@gmail.com>
2024-06-12 19:27:01 +0300

    create branch students

--- src/main/java/org/Lukash/Jack.java
+++ src/main/java/org/Lukash/Jack.java
@@ -3,5 +3,6 @@ package org.Lukash;
 public class Jack {
     public static void main(String[] args) {
         System.out.println("Hallo");
+        System.out.println("Create branch students");
     }
 }
