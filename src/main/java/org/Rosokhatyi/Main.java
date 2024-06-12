ee726eee16155d3a27d933be2c505067e891e2c3
Nikita33246 <nikntac@gmail.com>
2024-06-12 20:34:15 +0200

    Hello

--- src/main/java/org/Rosokhatyi/Main.java
+++ src/main/java/org/Rosokhatyi/Main.java
@@ -3,5 +3,6 @@ package org.Rosokhatyi;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("Good Night!");
     }
 }
