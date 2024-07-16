dd73e94f448496d401ca6dc65fcd6ec46a7c7ec8
Fryzero <hiperxowner@gmail.com>
2024-07-16 23:49:01 +0200

    Added TimeConverter for Santa

--- src/main/java/org/Balandiukov/Main.java
+++ src/main/java/org/Balandiukov/Main.java
@@ -3,5 +3,6 @@ package org.Balandiukov;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi there :3");
+        TimeConverter.convert(20000000);
     }
 }
